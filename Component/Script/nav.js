d95c3e22cec98b5f97608e0561f812b1ef11931f
Asad-Zaidi <syedasad1410@gmail.com>
2023-12-18 22:17:21 +0500

    local Storage added

--- Component/Script/nav.js
+++ Component/Script/nav.js
@@ -1,9 +1,20 @@
-document.querySelector('.navbar-btn').addEventListener('click', function () {
+document.querySelector('.navbar-btn').addEventListener('click', function() {
     document.querySelector('.navbar ul').classList.toggle('show');
 });
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     const darkModeToggler = document.querySelector('.dark-mode-toggler');
-    darkModeToggler.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-}
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    darkModeToggler.textContent = isDarkMode ? '☀️' : '🌙';
+
+    sessionStorage.setItem('colorMode', isDarkMode ? 'dark' : 'light');
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    const storedColorMode = sessionStorage.getItem('colorMode');
+    if (storedColorMode === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.querySelector('.dark-mode-toggler').textContent = '☀️';
+    }
+});
